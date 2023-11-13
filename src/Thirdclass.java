ba3300aa4fabd68f7045f3ca1e355c54a86bea22
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-13 22:06:33 +0600

    Update Thirdclass.java

--- src/Thirdclass.java
+++ src/Thirdclass.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Thirdclass {
+class ThirdClass extends Figure implements Printable {
+    public ThirdClass(String name) {
+        super(name);
+    }
+
+    public ThirdClass() {
+
+    }
+    // Реализация для ThirdClass с его специфичными полями
+
+    @Override
+    public void print() {
+        // Реализация для вывода информации о ThirdClass
+    }
+
+    @Override
+    public int calculatePerimeter() {
+        return 0;
+    }
+
+    @Override
+    public String draw() {
+        return null;
+    }
 }
