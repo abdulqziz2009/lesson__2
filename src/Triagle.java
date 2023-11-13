a836715c9717003a3341c9d34d19b06da39fcdca
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-13 22:06:58 +0600

    Update Triagle.java

--- src/Triagle.java
+++ src/Triagle.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Triagle {
+class Triangle extends Figure {
+    // Дополнительные поля и методы, специфичные для треугольника
+
+    public Triangle(String name) {
+        super(name);
+    }
+
+    @Override
+    public int calculatePerimeter() {
+        // Реализация расчета периметра треугольника
+        return 0;
+    }
+
+    @Override
+    public String draw() {
+        // Реализация рисования треугольника
+        return null;
+    }
 }
