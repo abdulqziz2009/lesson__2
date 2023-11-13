e7978438f52de6ddf2452563274c03dffa33814e
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-13 22:05:43 +0600

    Update SecondClass.java

--- src/SecondClass.java
+++ src/SecondClass.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class SecondClass {
+class SecondClass extends Figure implements Printable {
+    public SecondClass(String name) {
+        super(name);
+    }
+
+    public SecondClass() {
+        super();
+    }
+    // Реализация для SecondClass с его специфичными полями
+
+    @Override
+    public void print() {
+        // Реализация для вывода информации о SecondClass
+    }
+
+    @Override
+    public int calculatePerimeter() {
+        return 0;
+    }
+
+    @Override
+    public String draw() {
+        return null;
+    }
 }
