19f8e990b956cc7bedc847dd00c48ef71c7e8489
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-13 22:05:11 +0600

    Update Ractangle.java

--- src/Ractangle.java
+++ src/Ractangle.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Ractangle {
+class Rectangle extends Figure {
+    // Дополнительные поля и методы, специфичные для прямоугольника
+
+    public Rectangle(String name) {
+        super(name);
+    }
+
+    @Override
+    public int calculatePerimeter() {
+        // Реализация расчета периметра прямоугольника
+        return 0;
+    }
+
+    @Override
+    public String draw() {
+        // Реализация рисования прямоугольника
+        return null;
+    }
 }
