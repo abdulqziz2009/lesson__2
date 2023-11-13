184eaaa0f6a23ac1a5d6e53e141e1e6ce56e6ce8
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-13 22:06:07 +0600

    Update Squpe.java

--- src/Squpe.java
+++ src/Squpe.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Squpe {
+class Square extends Figure {
+    // Дополнительные поля и методы, специфичные для квадрата
+
+    public Square(String name) {
+        super(name);
+    }
+
+    @Override
+    public int calculatePerimeter() {
+        // Реализация расчета периметра квадрата
+        return 0;
+    }
+
+    @Override
+    public String draw() {
+        // Реализация рисования квадрата
+        return null;
+    }
 }
