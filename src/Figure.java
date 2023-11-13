ad20c544b5d77ce9545c8912a01c4471b93f4080
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-13 22:03:24 +0600

    Update Figure.java

--- src/Figure.java
+++ src/Figure.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Figure {
+abstract class Figure {
+    private String name;
+
+    public Figure(String name) {
+        this.name = name;
+    }
+
+    public Figure() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract int calculatePerimeter();
+
+    public abstract String draw();
 }
+
+
+
